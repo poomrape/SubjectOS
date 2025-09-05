24f9e941cd96bb9980d965cbf97d4f237f66e7f0
Poomrapee <poomrapee.bun@gmail.com>
2025-09-05 09:57:15 +0700

    update fork

--- myfork/fork.c
+++ myfork/fork.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <sys/wait.h>
 int main(){
 	pid_t pids[10];
-	int n=10;
-	int i =0;
+	int i ;
 
-	for(i=0;n<10;i++){
+	for(i=0;i<10;i++){
 		pids[i]=fork();
 
-		if(pid[i] < 0){
+		if(pids[i] < 0){
 			printf("fork failed");
 
-		}else if(pid[i] ==0){
-
+		}else if(pids[i] ==0){
+			printf("My child number:%d\n",i);
+			exit(0);
+		}
+	}
+	for(i=0;i<10;i++){
+		wait(NULL);
+	}
+	printf("My Parents Terminated ");
+}
 	
